1ca6b6f8068b6b98d3f83cdc06f4268c73a546f9
lawtondy <lawtondy@umich.edu>
2018-11-07 22:05:28 -0800

    added click events to each of the names, tried to get a simple riot api get request to fire, but ran into issues with cross-origin policy

--- src/app/team-list/team-list.component.ts
+++ src/app/team-list/team-list.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-team-list',
   templateUrl: './team-list.component.html',
   styleUrls: ['./team-list.component.css']
 })
+
 export class TeamListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // fill in the url with whatever you want to call plus your dev api key
+  url = '';
+  // need to fix issue with access control origin policy
+  // currently can't request riot api because it's different origin than localhost (obviously)
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      'Access-Control-Allow-Origin': '*'
+    })
+  };
 
   ngOnInit() {
   }
 
+  findSummoner(name : string): any {
+    console.log(this.httpOptions);
+    let response = this.http.request("GET", this.url, this.httpOptions).subscribe(
+      data => {
+        console.log("Get Request is successful ", data);
+    });
+    alert(name);
+  }
 }
